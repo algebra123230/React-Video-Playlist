22ae2e08dc0e01548a8da73ff86f15a334fa7822
Damien Jiang <jiangtyd@gmail.com>
2021-06-02 15:27:29 -0700

    Updates

---
+++ precache-manifest.eee671f2543c33b24afb8765dc8479a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe3a794699a82e901fca954d0f0c90a",
+    "url": "/React-Video-Playlist/index.html"
+  },
+  {
+    "revision": "1722aef72199f7015911",
+    "url": "/React-Video-Playlist/static/css/main.2a59c1c6.chunk.css"
+  },
+  {
+    "revision": "5525f7a19dd095b99ae2",
+    "url": "/React-Video-Playlist/static/js/2.e509a624.chunk.js"
+  },
+  {
+    "revision": "0f280e3aa3501f95e0c304afbb9318d8",
+    "url": "/React-Video-Playlist/static/js/2.e509a624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1722aef72199f7015911",
+    "url": "/React-Video-Playlist/static/js/main.617af29f.chunk.js"
+  },
+  {
+    "revision": "94a9a525e34022d251df",
+    "url": "/React-Video-Playlist/static/js/runtime-main.207d2c50.js"
+  }
+]);
